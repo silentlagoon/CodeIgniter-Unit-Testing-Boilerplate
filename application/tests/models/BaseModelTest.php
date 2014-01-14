4ae8fdf2caeea69afd34950569cbbd3dba6ed9ea
Dmitry Kurilenko <silentlagoon@gmail.com>
2014-01-14 19:12:10 +0400

    Database mock added to constructor

--- application/tests/models/BaseModelTest.php
+++ application/tests/models/BaseModelTest.php
@@ -13,6 +13,9 @@ class BaseModelTest extends CIUnit_TestCase
         parent::setUp();
         $this->CI->load->model('Base_Model');
         $this->base_model = $this->CI->Base_Model;
+        $this->mock = $this->getMockBuilder('CI_DB_active_record')
+            ->disableOriginalConstructor()
+            ->getMock();
     }
 
     public function tearDown()
@@ -22,17 +25,12 @@ class BaseModelTest extends CIUnit_TestCase
 
     public function test_create_calls_db_insert_with_correct_params()
     {
-        $mock = $this->getMockBuilder('CI_DB_active_record')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $mock->expects($this->any())
+        $this->mock->expects($this->any())
             ->method('insert')
             ->with($this->equalTo('table'), $this->equalTo(array('id' => 9)));
-
         $this->base_model->_table = 'table';
-        $this->base_model->db = $mock;
-
-        $this->base_model->create(array('id' => 7));
+        $this->base_model->db = $this->mock;
+        $this->base_model->create(array('id' => 9));
     }
 
 
