ce041aabd3d70432906783ccdc2fbf512770ab44
Dmitry Kurilenko <silentlagoon@gmail.com>
2014-01-15 15:08:22 +0400

    added tests for find_one and create methods of base model. added coverage

--- application/models/base_model.php
+++ application/models/base_model.php
@@ -7,12 +7,11 @@ class Base_model extends CI_Model
         parent::__construct();
     }
 
-
     function find_one($params, $options = array())
     {
         $options = array_merge(
             array(
-                'result_as_array' => FALSE,
+                'return_type' => 'object',
                 'table' => $this->_table
             ),$options);
 
@@ -20,25 +19,23 @@ class Base_model extends CI_Model
         if (is_int($params)) {
             $query = $this->db->get_where($options['table'], array('id' => $params), 1);
         }
-        if (is_array($params)) {
+        elseif (is_array($params)) {
             $query = $this->db->get_where($options['table'], $params, 1);
+        } else {
+            throw new InvalidArgumentException('params can be integer or array only. Input was: '.$params);
         }
 
-        if ( ! is_null($query)) {
-            if ($options['result_as_array'] === TRUE) {
-                $result = $query->result_array();
-            } else {
-                $result = $query->result();
-            }
-            return count($result) > 0 ? $result[0] : NULL;
+        if (is_null($query)) {
+            throw new UnexpectedValueException('query parameter cannot be NULL');
         }
 
-        throw new InvalidArgumentException('params can be integer or array only. Input was: '.$params);
+        $result = $query->result($options['return_type']);
+        return count($result) > 0 ? $result[0] : NULL;
     }
 
     function find_one_as_array($params, $options = array())
     {
-        $options['result_as_array'] = TRUE;
+        $options['return_type'] = 'array';
         return $this->find_one($params, $options);
     }
 
@@ -94,6 +91,5 @@ class Base_model extends CI_Model
     {
         $options['table'] = $table;
         return $this->find_all($params, $options);
-        //return $this->db->get_where($table, array('id_kogo' => $user_id, 'id_kto !=' => $user_id))->result();
     }
 }
--- application/tests/models/BaseModelTest.php
+++ application/tests/models/BaseModelTest.php
@@ -7,10 +7,12 @@
 class BaseModelTest extends CIUnit_TestCase
 {
     private $base_model;
+    private $mock;
 
     public function setUp()
     {
         parent::setUp();
+        $this->mock = NULL;
         $this->CI->load->model('Base_Model');
         $this->base_model = $this->CI->Base_Model;
         $this->mock = $this->getMockBuilder('CI_DB_active_record')
@@ -21,38 +23,203 @@ class BaseModelTest extends CIUnit_TestCase
     public function tearDown()
     {
         parent::tearDown();
+        $this->mock = NULL;
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function test_create_throws_invalid_argument_exception()
+    {
+        $this->base_model->create('parameter');
     }
 
     public function test_create_calls_db_insert_with_correct_params()
     {
         $this->mock->expects($this->any())
             ->method('insert')
-            ->with($this->equalTo('table'), $this->equalTo(array('id' => 9)));
+            ->with(
+                $this->equalTo('table'),
+                $this->equalTo(array('id' => 9))
+            );
         $this->base_model->_table = 'table';
         $this->base_model->db = $this->mock;
         $this->base_model->create(array('id' => 9));
     }
 
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function test_find_one_throw_invalid_argument_exception()
+    {
+        $this->base_model->find_one('parameter');
+    }
 
-//    public function test_int_value_find_one_method()
-//    {
-//        $this->_pcm->_table = 'test';
-//        return $this->_pcm->find_one($this->integer);
-//    }
-//
-//    /**
-//     * @depends test_array_value_find_one_method
-//     */
-//    public function test_if_array_value_is_empty_find_one_method($condition)
-//    {
-//        $this->assertNotEmpty($condition);
-//    }
-//
-//    /**
-//     * @depends test_int_value_find_one_method
-//     */
-//    public function test_if_int_value_is_empty_find_one_method($condition)
-//    {
-//        $this->assertNotEmpty($condition);
-//    }
+    /**
+     * @expectedException UnexpectedValueException
+     */
+    public function test_find_one_throws_unexpected_value_exception_if_query_is_null()
+    {
+        $this->mock->expects($this->any())->method('get_where');
+
+        $this->base_model->_table = 'table';
+        $this->base_model->db = $this->mock;
+        $this->base_model->find_one(9);
+    }
+
+    /**
+     * @expectedException UnexpectedValueException
+     */
+    public function test_find_one_calls_get_where_with_correct_parameters_when_integer_is_passed()
+    {
+        $this->mock->expects($this->any())
+            ->method('get_where')
+            ->with(
+                $this->equalTo('table'),
+                $this->equalTo(array('id' => 9)),
+                $this->equalTo(1)
+            );
+
+        $this->base_model->_table = 'table';
+        $this->base_model->db = $this->mock;
+        $this->base_model->find_one(9);
+    }
+
+    /**
+     * @expectedException UnexpectedValueException
+     */
+    public function test_find_one_calls_get_where_with_correct_parameters_when_array_is_passed()
+    {
+        $this->mock->expects($this->any())
+            ->method('get_where')
+            ->with(
+                $this->equalTo('table'),
+                $this->equalTo(array('name' => 'Alex')),
+                $this->equalTo(1)
+            );
+
+        $this->base_model->_table = 'table';
+        $this->base_model->db = $this->mock;
+        $this->base_model->find_one(array('name' => 'Alex'));
+    }
+
+    /**
+     * @expectedException UnexpectedValueException
+     */
+    public function test_find_one_calls_get_where_with_table_from_options_when_incoming_params_is_integer()
+    {
+        $this->mock->expects($this->any())
+            ->method('get_where')
+            ->with(
+                $this->equalTo('another_table'),
+                $this->equalTo(array('id' => 9)),
+                $this->equalTo(1)
+            );
+
+        $this->base_model->db = $this->mock;
+        $this->base_model->find_one(9, array('table' => 'another_table'));
+    }
+
+    /**
+     * @expectedException UnexpectedValueException
+     */
+    public function test_find_one_calls_get_where_with_table_from_options_when_incoming_params_is_array()
+    {
+        $this->mock->expects($this->any())
+            ->method('get_where')
+            ->with(
+                $this->equalTo('another_table'),
+                $this->equalTo(array('name' => 'Alex')),
+                $this->equalTo(1)
+            );
+
+        $this->base_model->db = $this->mock;
+        $this->base_model->find_one(array('name' => 'Alex'), array('table' => 'another_table'));
+    }
+
+    public function test_find_one_calls_result_with_type_equals_array()
+    {
+        $query_mock = $this->getMockBuilder('Fake_CI_DB_result')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $query_mock->expects($this->any())
+            ->method('result')
+            ->with($this->equalTo('array'));
+            //->will($this->returnValue(array(100)));
+
+        $this->mock->expects($this->any())
+                ->method('get_where')
+                ->will($this->returnValue($query_mock));
+
+        $this->base_model->_table = 'table';
+        $this->base_model->db = $this->mock;
+        $this->base_model->find_one(9, array('return_type' => 'array'));
+        //$this->assertEquals(100, $result);
+    }
+
+    public function test_find_one_calls_result_with_type_equals_object()
+    {
+        $query_mock = $this->getMockBuilder('Fake_CI_DB_result')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $query_mock->expects($this->any())
+            ->method('result')
+            ->with($this->equalTo('object'));
+
+        $this->mock->expects($this->any())
+            ->method('get_where')
+            ->will($this->returnValue($query_mock));
+
+        $this->base_model->_table = 'table';
+        $this->base_model->db = $this->mock;
+        $this->base_model->find_one(9);
+    }
+
+    public function test_find_one_returns_null()
+    {
+        $query_mock = $this->getMockBuilder('Fake_CI_DB_result')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $query_mock->expects($this->any())
+            ->method('result')
+            ->will($this->returnValue(array()));
+
+        $this->mock->expects($this->any())
+            ->method('get_where')
+            ->will($this->returnValue($query_mock));
+
+        $this->base_model->_table = 'table';
+        $this->base_model->db = $this->mock;
+        $result = $this->base_model->find_one(9);
+        $this->assertNull($result);
+    }
+
+    public function test_find_one_returns_correct_value()
+    {
+        $query_mock = $this->getMockBuilder('Fake_CI_DB_result')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $query_mock->expects($this->any())
+            ->method('result')
+            ->will($this->returnValue(array(100, 200)));
+
+        $this->mock->expects($this->any())
+            ->method('get_where')
+            ->will($this->returnValue($query_mock));
+
+        $this->base_model->_table = 'table';
+        $this->base_model->db = $this->mock;
+        $result = $this->base_model->find_one(9);
+        $this->assertEquals(100, $result);
+    }
+}
+
+class Fake_CI_DB_result
+{
+    function result() {}
+    function result_array() {}
 }
